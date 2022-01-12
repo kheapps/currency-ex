357db8ff83c2302807af5ccf83d07b08ffea3549
B4H44 <mansour.b@kheapps.com>
2022-01-12 19:16:23 +0100

    update reverse button icon on small screen

--- src/components/CurrencyConverter.vue
+++ src/components/CurrencyConverter.vue
@@ -6,7 +6,7 @@
       :disabled="false"
       @set-amount="setAmount"
     />
-    <div class="reverse-btn" @click="reverseCurrencies">
+    <div v-if="!isSmallScreen" class="reverse-btn" @click="reverseCurrencies">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="16"
@@ -18,6 +18,21 @@
         <path
           fill-rule="evenodd"
           d="M1 11.5a.5.5 0 0 0 .5.5h11.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 11H1.5a.5.5 0 0 0-.5.5zm14-7a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H14.5a.5.5 0 0 1 .5.5z"
+        />
+      </svg>
+    </div>
+    <div v-else class="reverse-btn" @click="reverseCurrencies">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        class="bi bi-arrow-down-up"
+        viewBox="0 0 16 16"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M11.5 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L11 2.707V14.5a.5.5 0 0 0 .5.5zm-7-14a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V1.5a.5.5 0 0 1 .5-.5z"
         />
       </svg>
     </div>
@@ -46,6 +61,7 @@ export default {
       currencyFactor: 0,
       amountEntered: 1.0,
       currencyFactorFetchedDate: Date,
+      isSmallScreen: false,
     };
   },
   inject: ["getFromTo"],
@@ -90,9 +106,16 @@ export default {
     setAmount(amount) {
       this.amountEntered = amount;
     },
+    sizeChanged(e) {
+      this.isSmallScreen = e.target.innerWidth <= 1050;
+    },
   },
   created() {
     this.getCurrencyFactor();
+    window.addEventListener("resize", this.sizeChanged);
+  },
+  unmounted() {
+    window.removeEventListener("resize", this.sizeChanged);
   },
 };
 </script>
